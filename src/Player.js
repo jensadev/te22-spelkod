d0200ef64a4d8f0fd4e26681d78fb5ad08431bb0
Jens Andreasson <jens.andreasson@ntig.se>
2024-11-11 10:56:12 +0100

    juice

--- src/Player.js
+++ src/Player.js
@@ -9,6 +9,7 @@ export default class Player extends GameObject {
     this.speedY = 0
     this.maxSpeedX = 1
     this.maxSpeedY = 5
+    this.color = "255, 0, 0"
   }
 
   update (deltaTime) {
@@ -39,5 +40,12 @@ export default class Player extends GameObject {
 
     this.y += this.speedY
     this.x += this.speedX
+  }
+
+  draw(ctx) {
+      ctx.fillStyle = `rgba(${this.color}, 0.8)`
+      ctx.fillRect(this.x + this.speedX, this.y, this.width, this.height)
+      ctx.fillStyle = `rgba(${this.color}, 1)`
+      ctx.fillRect(this.x, this.y, this.width, this.height)
   }
 }
